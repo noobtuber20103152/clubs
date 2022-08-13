362de35502f7aabfe67757d78848c2413030fe66
ujjawal <ujjawalrachhoya100@gmail.com>
2022-08-13 15:03:01 +0530

    sc-st-cell page

--- Homepg/app.js
+++ Homepg/app.js
@@ -74,9 +74,9 @@ function isInViewPort(element) {
     bounding.top >= 0 &&
     bounding.left >= 0 &&
     bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth) &&
+    (window.innerWidth || document.documentElement.clientWidth) &&
     bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight)
+    (window.innerHeight || document.documentElement.clientHeight)
   ) {
     console.log('In the viewport! :)')
     return true
